d684a48575c7c16b2573d622af95a755b55c1473
T.J. Kyner <tjkyner@gmail.com>
2022-06-17 17:40:48 -0400

    navbar placeholders

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,14 +4,14 @@ import {Container, Grid, theme, Text} from "@nextui-org/react";
 const Footer = () => {
 
     return (
-        <div style={{background: theme.colors.accents9.computedValue, marginTop: "auto"}}>
+        <div style={{background: theme.colors.accents1.computedValue, marginTop: "auto"}}>
 
             <Container>
 
                 <Grid.Container gap={2} style={{marginBottom: 0, paddingBottom: 0}}>
 
                     <Grid>
-                        <Text color={"white"}>Built with 💜 by Tokr Labs</Text>
+                        <Text>Built with 💜 by Tokr Labs</Text>
                     </Grid>
 
                 </Grid.Container>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,20 +1,30 @@
 import React from "react";
-import {Container, Grid, theme} from "@nextui-org/react";
+import {Button, Container, Grid, theme} from "@nextui-org/react";
 import {WalletMultiButton} from "@solana/wallet-adapter-react-ui";
+import Link from "next/link";
 
 const Navbar = () => {
 
     return (
-        <div style={{background: theme.colors.accents9.computedValue}}>
+        <div style={{background: theme.colors.accents1.computedValue}}>
 
             <Container>
 
-                <Grid.Container gap={2}>
+                <Grid.Container gap={2} style={{margin: "0 -12px", padding: "0 12px"}}>
 
-                    <Grid xs={12} justify={"flex-end"}>
+                    <Grid xs={4}>
+                        <Link href={"/"}>
+                            <Button auto>Logo Placeholder</Button>
+                        </Link>
+                    </Grid>
 
+                    <Grid xs={4}>
+                        <Button flat>About</Button>
+                        <Button flat>Team</Button>
+                    </Grid>
+
+                    <Grid xs={4} justify={"flex-end"}>
                         <WalletMultiButton/>
-
                     </Grid>
 
                 </Grid.Container>
