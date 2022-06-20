3bda239f4ce7a3abf73b9b014f015d9d5cf69301
BDANG <heybdang@gmail.com>
2022-06-20 10:44:44 -0400

    signable metaplex SDK

--- pages/api/constants.ts
+++ pages/api/constants.ts
@@ -1,12 +1,15 @@
 // A place for defining constants or reusable objects
-// an API should not be defined, otherwise it will be exposed publicly
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { Metaplex } from "@metaplex-foundation/js";
-import { Connection, clusterApiUrl } from "@solana/web3.js";
+// an API should NOT be defined, otherwise it will be exposed publicly
+import { keypairIdentity, Metaplex } from "@metaplex-foundation/js";
+import { Connection, clusterApiUrl, Keypair } from "@solana/web3.js";
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import * as bs58 from "bs58";
 
 export const connection = new Connection(clusterApiUrl(WalletAdapterNetwork.Devnet));
 export const metaplex = new Metaplex(connection);
+
+const adminWallet = Keypair.fromSecretKey(bs58.decode(process.env.SOLANA_PRIVATE_KEY!));
+export const signable_metaplex = new Metaplex(connection).use(keypairIdentity(adminWallet));
 
 // generic response types
 export type MissingArgs = {
