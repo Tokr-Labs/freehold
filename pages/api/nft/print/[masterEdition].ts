1e642ba0c5a3e05ad860653ddd6b776f6578d0f1
BDANG <heybdang@gmail.com>
2022-06-20 10:44:56 -0400

    API endpoint for managing prints

---
+++ pages/api/nft/print/[masterEdition].ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Nft, PrintNewEditionOutput } from "@metaplex-foundation/js";
+import { PublicKey } from "@solana/web3.js";
+import { metaplex, signable_metaplex } from "../../constants";
+
+type Data = {
+    masterEdition: string | string[],
+    nft: Nft,
+    error?: string
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    const { query: { masterEdition }, method } = req;
+    const masterEditionKey = new PublicKey(masterEdition);
+    switch (method) {
+        case 'GET':
+            // @TODO: this route should actually return all print editions
+            // we may need to crawl the chain, or use `findAllByCreator(adminWallet)`
+            const nft: Nft = await metaplex.nfts().findByMint(masterEditionKey);
+            res.status(200).json({ masterEdition, nft });
+            break;
+        case 'POST':
+            // @TODO: perform security checks as priveleged users will be spending admin SOL to mint the prints
+            const printNft: any = await signable_metaplex.nfts().printNewEdition(masterEditionKey);
+            res.status(200).json({ masterEdition, nft: printNft });
+            break;
+        default:
+            res.setHeader('Allow', ['GET', 'POST']);
+            res.status(405).end(`Method ${method} Not Allowed`);
+    }
+
+}
