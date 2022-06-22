915a890705c1b23bfecb14955e3fd93f2f81e77e
BDANG <heybdang@gmail.com>
2022-06-22 14:07:29 -0400

    reorganized network usage on the API side, with optional network selection via parameters

--- pages/api/nft/owned.ts
+++ pages/api/nft/owned.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { Nft } from "@metaplex-foundation/js";
 import { PublicKey } from "@solana/web3.js";
 import { metaplex, MissingArgs } from "../constants";
+import { getMetaplex, getSolanaConnection } from '../util';
 
 type Data = {
     user: string,
@@ -17,8 +18,15 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data | MissingArgs>
 ) {
+    // parse query params into variables
     const { query: { user, collection }, method } = req;
     const metadata: boolean = req.query.metadata === 'true';
+    const network = req.query.network as string;
+
+    // use specific network, if provided by the request. otherwise use the default
+    const mx = network ? getMetaplex(getSolanaConnection(network)) : metaplex;
+
+    // handle the request -- fetching user's NFTs w/ optional collection filter
     switch (method) {
         case 'GET':
             // validate that a user was provided
@@ -28,7 +36,7 @@ export default async function handler(
             }
             
             // obtain user's list of metaplex NFTs
-            var nfts: Nft[] = await metaplex.nfts().findAllByOwner(new PublicKey(user));
+            var nfts: Nft[] = await mx.nfts().findAllByOwner(new PublicKey(user));
             
             // if a collection was provided, filter the list of NFTs for the collection
             if (collection) {
