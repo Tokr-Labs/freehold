205bd02cd178457c8f8743e44e5b420163d06820
T.J. Kyner <78994885+tjkyner@users.noreply.github.com>
2022-06-23 15:46:15 -0400

    UI network switching (#22)
    * dropdown for network switching
    
    * wallet adapter button styling
    
    * fix for key selection

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-import {Button, Container, Grid, theme} from "@nextui-org/react";
+import React, {useContext, useState} from "react";
+import {Button, Container, Dropdown, Grid, Spacer, theme} from "@nextui-org/react";
 import {WalletMultiButton} from "@solana/wallet-adapter-react-ui";
 import Link from "next/link";
+import {NetworkContext} from "../contexts/network-context";
+import {WalletAdapterNetwork} from "@solana/wallet-adapter-base";
 
 const Navbar = () => {
+
+    const {setNetwork} = useContext(NetworkContext)
+
+    const [selected, setSelected] = useState("devnet");
+
+    const [selectedKeys, setSelectedKeys] = useState(new Set(["devnet"]));
 
     return (
         <div style={{background: theme.colors.accents0.computedValue}}>
@@ -24,7 +32,38 @@ const Navbar = () => {
                     </Grid>
 
                     <Grid xs={4} justify={"flex-end"}>
+
+                        <Dropdown>
+                            <Dropdown.Button flat css={{tt: "capitalize"}}>
+                                {selected}
+                            </Dropdown.Button>
+                            <Dropdown.Menu
+                                aria-label="network selection"
+                                disallowEmptySelection
+                                selectionMode="single"
+                                selectedKeys={selectedKeys}
+                                onSelectionChange={keys => {
+                                    // @ts-ignore
+                                    setSelectedKeys(keys)
+
+                                    // @ts-ignore
+                                    const [selection] = keys
+                                    setSelected(selection)
+
+                                    selection === "devnet"
+                                        ? setNetwork(WalletAdapterNetwork.Devnet)
+                                        : setNetwork(WalletAdapterNetwork.Mainnet)
+                                }}
+                            >
+                                <Dropdown.Item key="devnet">Devnet</Dropdown.Item>
+                                <Dropdown.Item key="mainnet">Mainnet</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+
+                        <Spacer x={1}/>
+
                         <WalletMultiButton/>
+
                     </Grid>
 
                 </Grid.Container>
