c9a528c9533bbd6b643cde68d9270d0545831a22
BDANG <heybdang@gmail.com>
2022-06-25 20:02:37 -0400

    POST endpoint for getting a serialized tx of pfp-setting

--- pages/api/pfp.ts
+++ pages/api/pfp.ts
@@ -1,8 +1,10 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {getSolanaConnection} from "./util";
-import {PublicKey} from "@solana/web3.js";
+import {PublicKey, Signer} from "@solana/web3.js";
+import {getAssociatedTokenAddress} from "@solana/spl-token";
 import {corsMiddleware} from "../../utils/middleware";
-import {getProfilePicture} from "@solflare-wallet/pfp";
+import {createSetProfilePictureTransaction, getProfilePicture} from "@solflare-wallet/pfp";
+import * as bs58 from "bs58";
 
 export default async function handler(
     req: NextApiRequest,
@@ -32,9 +34,25 @@ export default async function handler(
 
             res.status(200).json(pfp)
             break;
+        // create a transaction for setting a PFP on solflare's pfp-program
+        // the transaction is encoded in base58 and returned to the client
+        case "POST":
+            let { body: { owner, mint } } = req;
+            owner = new PublicKey(owner);
+            mint = new PublicKey(mint);
 
+            const tokenAta = await getAssociatedTokenAddress(mint, owner)
+            const tx = await createSetProfilePictureTransaction(owner, mint, tokenAta);
+            
+            // need to be set otherwise serialization fails
+            tx.feePayer = owner;
+            tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+            
+            let tx_serialized = bs58.encode(tx.serializeMessage());
+            res.status(200).json({ tx_serialized });
+            break;
         default:
-            res.setHeader("Allow", ["GET"]);
+            res.setHeader("Allow", ["GET", "POST"]);
             res.status(405).end(`Method ${method} Not Allowed`);
 
     }
