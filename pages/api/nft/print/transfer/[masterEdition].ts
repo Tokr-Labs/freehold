69adbe2f490397ee1123aaa6aaa73bcadedb3df1
BDANG <heybdang@gmail.com>
2022-06-25 21:29:51 -0400

    add auth

--- pages/api/nft/print/transfer/[masterEdition].ts
+++ pages/api/nft/print/transfer/[masterEdition].ts
@@ -2,8 +2,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Nft, PrintNewEditionOutput } from "@metaplex-foundation/js";
 import { PublicKey, sendAndConfirmTransaction } from "@solana/web3.js";
-import { adminWallet, connection, metaplex, signable_metaplex } from "../../../constants";
+import { adminWallet, AuthorizationFailure, connection, metaplex, signable_metaplex } from "../../../constants";
 import { transferAdminNftTransaction } from '../../../../../library/nft/transfer';
+import { basicAuthMiddleware } from '../../../../../utils/middleware';
 
 type Data = {
     success: boolean,
@@ -18,19 +19,23 @@ type Data = {
 // creates a print NFT (copy) of the provided master NFT and transfers it to a new owner
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Data>
+    res: NextApiResponse<Data | AuthorizationFailure>
 ) {
     const { query: { masterEdition }, body: { to }, method } = req;
     const masterEditionKey = new PublicKey(masterEdition);
     switch (method) {
         case 'POST':
-            // @TODO: perform security checks as priveleged users will be spending admin SOL to mint the prints
-            // TODO: atomically mint the print NFT & transfer it to the new owner, to prevent excess prints
-            const printNft: any = await signable_metaplex.nfts().printNewEdition(masterEditionKey);
-            const tx = await transferAdminNftTransaction(printNft.nft.mint, new PublicKey(to));
-            sendAndConfirmTransaction(connection, tx, [adminWallet]);
+            const authorized = basicAuthMiddleware(req);
+            if (authorized) {
+                // TODO: atomically mint the print NFT & transfer it to the new owner, to prevent excess prints
+                const printNft: any = await signable_metaplex.nfts().printNewEdition(masterEditionKey);
+                const tx = await transferAdminNftTransaction(printNft.nft.mint, new PublicKey(to));
+                sendAndConfirmTransaction(connection, tx, [adminWallet]);
 
-            res.status(200).json({ success: true, message: `Minted ${printNft.nft.mint} to ${to}`, nft: printNft });
+                res.status(200).json({ success: true, message: `Minted ${printNft.nft.mint} to ${to}`, nft: printNft });
+            } else {
+                res.status(401).json({ message: 'Authorization Failed', error: 'Invalid authorization' });
+            }
             break;
         default:
             res.setHeader('Allow', ['POST']);
