37180b40fe675a2f4472349e2489e075a9e1686d
BDANG <heybdang@gmail.com>
2022-06-27 11:04:01 -0400

    remove repeat code in favor of reusable function

--- pages/api/nft/transfer.ts
+++ pages/api/nft/transfer.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { Nft } from "@metaplex-foundation/js";
 import { PublicKey, sendAndConfirmTransaction, Transaction, TransactionInstruction } from "@solana/web3.js";
 import { connection, metaplex, adminWallet, Success, AuthorizationFailure, AUTHORIZATION_FAILED } from "../constants";
-import { getTokenTransferInstructions } from '../../../library/nft/transfer';
+import { getTokenTransferInstructions, transferAdminNftTransaction } from '../../../library/nft/transfer';
 import { getAssociatedTokenAddress } from '@solana/spl-token';
 import {basicAuthMiddleware, corsMiddleware} from '../../../utils/middleware';
 
@@ -26,22 +26,10 @@ export default async function handler(
                 // obtain NFT to transfer
                 const nft: Nft = await metaplex.nfts().findByMint(new PublicKey(token));
 
-                // associated token account for the NFT & admin wallet
-                const ata = await getAssociatedTokenAddress(nft.mint, adminWallet.publicKey);
+                // construct tx for transferring it to the destination
+                const tx = await transferAdminNftTransaction(nft.mint, new PublicKey(to));
 
-                // creates destination ATA (if it doesn't exist)
-                // transfer NFT from admin ATA to destination ATA
-                const ixs: TransactionInstruction[] = await getTokenTransferInstructions({
-                    connection,
-                    payer: adminWallet,
-                    source: ata,
-                    destination: new PublicKey(to),
-                    mint: nft.mint,
-                    amount: 1
-                });
-
-                // sign & send transaction
-                const tx = new Transaction().add(...ixs);
+                // sign transaction with admin wallet & send it
                 await sendAndConfirmTransaction(connection, tx, [adminWallet]);
                 res.status(200).json({
                     success: true,
