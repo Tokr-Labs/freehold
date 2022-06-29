1eff8928d16bd88e7bc20bae59a3ef61343d3fa9
BDANG <heybdang@gmail.com>
2022-06-29 13:35:04 -0400

    Misc Logging (#36)
    report admin wallet & rpc url

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { basicAuthMiddleware } from '../../utils/middleware';
+import { adminWallet, AuthorizationFailure, AUTHORIZATION_FAILED, connection } from './constants';
 
 type Data = {
-  name: string
+    public_key: string,
+    rpc_url: string,
 }
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+    req: NextApiRequest,
+    res: NextApiResponse<Data | AuthorizationFailure>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    const authorized = basicAuthMiddleware(req);
+    if (authorized) {
+        res.status(200).json({
+            public_key: adminWallet.publicKey.toBase58(),
+            rpc_url: connection.rpcEndpoint,
+        });
+    } else {
+        res.status(401).json(AUTHORIZATION_FAILED)
+    }
 }
