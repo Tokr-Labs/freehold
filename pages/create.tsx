4a1f1124bbad633a6d10a2e7cdb92307a415a513
T.J. Kyner <tjkyner@gmail.com>
2022-07-12 16:43:54 -0400

    ability to set a collection with auto verification

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,14 +1,28 @@
 import React, {useContext, useState} from "react";
 import {NextPage} from "next";
 import {Button, Card, Checkbox, Container, Grid, Image, Input, Spacer, Text, Textarea} from "@nextui-org/react";
-import {useWallet} from "@solana/wallet-adapter-react";
-import {CreateNftInput, Nft, useMetaplexFileFromBrowser, walletAdapterIdentity} from "@metaplex-foundation/js";
+import {useConnection, useWallet} from "@solana/wallet-adapter-react";
+import {
+    CreateNftInput, findMasterEditionV2Pda, findMetadataPda,
+    Nft, parseMetadataAccount, parseOriginalOrPrintEditionAccount,
+    TokenMetadataProgram,
+    useMetaplexFileFromBrowser,
+    walletAdapterIdentity
+} from "@metaplex-foundation/js";
 import PageWrapper from "../components/page-wrapper";
 import {MetaplexContext} from "../contexts/metaplex-context";
+import {PublicKey, Transaction} from "@solana/web3.js";
+import {
+    createSetAndVerifyCollectionInstruction,
+    createVerifyCollectionInstruction
+} from "@metaplex-foundation/mpl-token-metadata";
+import {Buffer} from "buffer";
+import {metaplex} from "./api/_constants";
 
 const Create: NextPage = () => {
 
     const walletAdapter = useWallet();
+    const {connection} = useConnection();
 
     // TODO - why does the wallet adapter identity need to be set again here?
     const mx = useContext(MetaplexContext).use(walletAdapterIdentity(walletAdapter));
@@ -16,6 +30,7 @@ const Create: NextPage = () => {
     const [name, setName] = useState<string>()
     const [symbol, setSymbol] = useState<string>()
     const [description, setDescription] = useState<string>()
+    const [collection, setCollection] = useState<string>()
     const [isMutable, setIsMutable] = useState<boolean>(true)
     const [image, setImage] = useState<File>()
     const [nft, setNft] = useState<Nft>()
@@ -31,10 +46,43 @@ const Create: NextPage = () => {
             seller_fee_basis_points: 0,
         })
 
+        console.log(`Uploaded metadata (URI: ${uri})`)
+
         const {nft} = await mx.nfts().create({
             uri: uri,
             isMutable
         } as CreateNftInput)
+
+        console.log(`Created NFT: ${nft.mint}`)
+
+        if (collection) {
+
+            const collectionNft = await mx.nfts().findByMint(new PublicKey(collection))
+
+            const nftMetadataAccount = await findMetadataPda(nft.mint)
+            const masterEditionAccount = await findMasterEditionV2Pda(collectionNft.mint)
+            const collectionMetadataAccount = await findMetadataPda(collectionNft.mint)
+
+            const tx = new Transaction()
+            tx.add(
+                createSetAndVerifyCollectionInstruction({
+                    metadata: nftMetadataAccount,
+                    collectionAuthority: collectionNft.updateAuthority,
+                    payer: mx.identity().publicKey,
+                    updateAuthority: collectionNft.updateAuthority,
+                    collectionMint: collectionNft.mint,
+                    collection: collectionMetadataAccount,
+                    collectionMasterEditionAccount: masterEditionAccount,
+                })
+            )
+
+            console.log("About to send transaction to set and verify collection")
+
+            await mx.rpc().sendAndConfirmTransaction(tx, [mx.identity()])
+                .then(response => console.log("Signature:", response.signature))
+                .catch(console.error)
+
+        }
 
         setNft(nft)
 
@@ -82,6 +130,16 @@ const Create: NextPage = () => {
                                 <Textarea label={"Description"} onChange={e => {
                                     setDescription(e.target.value)
                                 }}/>
+
+                                <Spacer y={1}/>
+
+                                <Input
+                                    label={"Collection"}
+                                    placeholder={"PublicKey of the associated collection NFT (optional)"}
+                                    onChange={e => {
+                                        setCollection(e.target.value)
+                                    }}
+                                />
 
                                 <Spacer y={1}/>
 
